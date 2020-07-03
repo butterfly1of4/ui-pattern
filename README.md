67b0a10c1dd2b31adafb08a5a9949a7b6a7337d4
butterfly1of4 <jhouck29@gmail.com>
2020-07-03 17:43:26 -0400

    api in

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ui-pattern
+Nasa api
+website: 
+
+
+const nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=IOPP2e1JpfEP1pCodZ23AiP522NPjqrsXSjdmPv7"
+const nasaApiKey = IOPP2e1JpfEP1pCodZ23AiP522NPjqrsXSjdmPv7
+
+
+
+
+
+
+Hewitt cooper
+const urlHC = "https://api.collection.cooperhewitt.org/rest/"
---
+++ pattern.js
@@ -0,0 +1,15 @@
+//NASA API
+const url = "https://api.nasa.gov/planetary/apod?api_key=IOPP2e1JpfEP1pCodZ23AiP522NPjqrsXSjdmPv7"
+const nasaApiKey = "IOPP2e1JpfEP1pCodZ23AiP522NPjqrsXSjdmPv7"
+const Api = 
+
+
+fetch(url, {headers: {"IOPP2e1JpfEP1pCodZ23AiP522NPjqrsXSjdmPv7": nasaApiKey }})
+    .then((res) => {
+        console.log("i worked", res)
+        return res.json();
+     })
+     .then((json) => {
+        json.url
+        console.log("json here", json)
+     })
